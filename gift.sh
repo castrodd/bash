2edc9183c1601af2493d99fb05afecaca1b6e84a
Daniel Castro <castrodd@gmail.com>
2017-08-23 20:26:20 -0400

    Added command line argument

--- gift.sh
+++ gift.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
 cd ~/Desktop
-mkdir gift
+
+args=("$@")
+if [[ $# -eq 1 ]] ; then
+  mkdir ${args[0]}
+else
+  mkdir default
+fi
+
 for file in * ;
 do
   if [[ -d $file ]] ; then
     :
   else
-    mv "$file" gift;
+    if [[ $# -eq 1 ]] ; then
+      mv "$file" ${args[0]}
+    else
+      mv "$file" default
+    fi
   fi
 done
